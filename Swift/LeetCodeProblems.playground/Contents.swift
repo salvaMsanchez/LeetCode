d7236b0c0647d68194d0ba0698c6383a71952660
Salva Moreno <morenosanchezsalva@gmail.com>
2024-07-06 18:48:40 +0200

    :heavy_plus_sign: Add Roman to Integer Problem

--- Swift/LeetCodeProblems.playground/Contents.swift
+++ Swift/LeetCodeProblems.playground/Contents.swift
@@ -72,8 +72,6 @@ solutionTwoSum.twoSum([3, 2, 4], 6)
 solutionTwoSum.twoSum2([3, 2, 4], 6)
 solutionTwoSum.twoSum3([3, 2, 4], 6)
 
-print("==========================")
-
 // -- 2. Palindrome Number --
 // * Difficulty: Easy
 // Given an integer x, return true if x is a palindrome, and false otherwise.
@@ -106,3 +104,98 @@ let solutionPalindrome: SolutionPalindrome = SolutionPalindrome()
 solutionPalindrome.isPalindrome(121)
 solutionPalindrome.isPalindrome(-121)
 solutionPalindrome.isPalindrome(10)
+
+// -- 3. Roman to Integer --
+// * Difficulty: Easy
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together.
+// 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+// Roman numerals are usually written largest to smallest from left to right.
+// However, the numeral for four is not IIII. Instead, the number four is written as IV.
+// Because the one is before the five we subtract it making four.
+// The same principle applies to the number nine, which is written as IX.
+// There are six instances where subtraction is used:
+// * I can be placed before V (5) and X (10) to make 4 and 9.
+// * X can be placed before L (50) and C (100) to make 40 and 90.
+// * C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+class SolutionRomanInteger {
+    func romanToInt(_ s: String) -> Int {
+        var result: Int = 0
+        var romanNumbers: [Character] = []
+        
+        s.forEach { romanNumbers.append($0) }
+        
+        for (romanIndex, romanNum) in romanNumbers.enumerated() {
+            switch romanNum {
+                case "I":
+                    result += 1
+                case "V":
+                    if romanIndex == 0 {
+                        result += 5
+                    } else if romanNumbers[romanIndex - 1] == "I" {
+                        result = (result - 1) + 4
+                    } else {
+                        result += 5
+                    }
+                case "X":
+                    if romanIndex == 0 {
+                        result += 10
+                    } else if romanNumbers[romanIndex - 1] == "I" {
+                        result = (result - 1) + 9
+                    } else {
+                        result += 10
+                    }
+                case "L":
+                    if romanIndex == 0 {
+                        result += 50
+                    } else if romanNumbers[romanIndex - 1] == "X" {
+                        result = (result - 10) + 40
+                    } else {
+                        result += 50
+                    }
+                case "C":
+                    if romanIndex == 0 {
+                        result += 100
+                    } else if romanNumbers[romanIndex - 1] == "X" {
+                        result = (result - 10) + 90
+                    } else {
+                        result += 100
+                    }
+                case "D":
+                    if romanIndex == 0 {
+                        result += 500
+                    } else if romanNumbers[romanIndex - 1] == "C" {
+                        result = (result - 100) + 400
+                    } else {
+                        result += 500
+                    }
+                case "M":
+                    if romanIndex == 0 {
+                        result += 1000
+                    } else if romanNumbers[romanIndex - 1] == "C" {
+                        result = (result - 100) + 900
+                    } else {
+                        result += 1000
+                    }
+                default:
+                    result += 0
+            }
+        }
+        return result
+    }
+}
+
+let solutionRomanInteger: SolutionRomanInteger = SolutionRomanInteger()
+solutionRomanInteger.romanToInt("III")
+solutionRomanInteger.romanToInt("LVIII")
+solutionRomanInteger.romanToInt("MCMXCIV")
